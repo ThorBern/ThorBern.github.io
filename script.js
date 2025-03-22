7b8495c9fe26b3c6db84bc2e82f631844b982b1d
ThorBern <bernhard.woods@gmail.com>
2025-03-21 22:44:53 -0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,6 @@
+// Example function for a button click
+function notifyUser() {
+  // You can connect this to a modal, an email subscription form,
+  // or redirect to a sign-up page, etc.
+  alert("You will be notified when Æsc goes live!");
+}
